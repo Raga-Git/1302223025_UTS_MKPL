0193bd531aad6cba9cdf47bb797356673f794a15
Raga-Git <ragadhitya@gmail.com>
2025-04-18 23:44:06 +0700

    Inisialisasi

---
+++ src/main/java/com/mycompany/_tax_calculator/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._tax_calculator;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
